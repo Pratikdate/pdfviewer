e6d8ffd52a8738679ee67fa3020513894a293c66
Pratikdate <pratikdate100@gmail.com>
2024-02-14 11:50:07 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:pdfviewer/viewer/pdfViewer.dart';
+import 'package:receive_intent/receive_intent.dart';
+import 'package:uri_to_file/uri_to_file.dart';
 
 void main() {
-  runApp(  MyApp());
+  runApp(  const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,29 +22,88 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
+
+  late File file;
+
+
+
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                PdfViewer()
+    IntentHandler();
+  }
+
+
+
+
+  void IntentHandler() async{
+    await _initReceiveIntent()?Timer(const Duration(seconds: 0),
+            () =>
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder:
+                    (context) =>
+
+                    PdfViewer(file: file))
             )
-        )
+    ):Timer(const Duration(seconds: 1),
+            () =>
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder:
+                    (context) =>
+                    PdfViewer())
+
+            )
     );
   }
+
+
+  Future<bool> _initReceiveIntent() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+
+      final receivedIntent = await ReceiveIntent.getInitialIntent();
+
+      if(receivedIntent!.isNotNull){
+
+        File file_ = await toFile(receivedIntent.data.toString());
+        file=file_;
+
+
+
+
+        return true;
+
+
+      }
+
+      return false;
+
+      // Validate receivedIntent and warn the user, if it is not correct,
+      // but keep in mind it could be `null` or "empty"(`receivedIntent.isNull`).
+    } on PlatformException {
+      // Handle exception
+      return false;
+    }
+  }
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/viewer/pdfViewer.dart
+++ lib/viewer/pdfViewer.dart
@@ -3,14 +3,15 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
-
-
+import 'package:receive_intent/receive_intent.dart';
 
 class PdfViewer extends StatefulWidget {
-  const PdfViewer({super.key});
-
+  const PdfViewer({super.key, this.file = null});
+  final file;
 
   @override
   State<PdfViewer> createState() => _PdfViewerState();
@@ -18,22 +19,52 @@ class PdfViewer extends StatefulWidget {
 
 class _PdfViewerState extends State<PdfViewer> {
   final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
-  bool isFilePicked=false;
+  bool isFilePicked = false;
   late Uint8List bytes;
-  PdfViewerController pdfViewerController= PdfViewerController();
+  PdfViewerController pdfViewerController = PdfViewerController();
   String? selectedTextLines;
-
-
-
+  bool isSearch = false;
+  TextEditingController Controller = TextEditingController();
+  dynamic prefs;
   @override
   void initState() {
     super.initState();
+
+    setState(() {
+      if (widget.file != null) {
+        bytes = widget.file.readAsBytesSync();
+        isFilePicked = true;
+        _loadCounter();
+
+      }
+    });
   }
+  Future<void> _loadCounter() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    prefs=_prefs;
+      setState(() {
+        pdfViewerController.jumpToPage(10);
+      });
+
+      //print('page no ${prefs.getInt(widget.file.path)}');
+
+
+
+
+  }
+
+  @override
+  void dispose() {
+    prefs.setInt(widget.file.path, pdfViewerController.pageNumber);
+
+    super.dispose();
+  }
+
 
   Future<dynamic> fetchAlbum(String text) async {
     try {
-      final response = await http.get(
-          Uri.parse('https://api.dictionaryapi.dev/api/v2/entries/en/$text'));
+      final response = await http.get(Uri.parse(
+          "https://api.dictionaryapi.dev/api/v2/entries/en/${text.replaceAll(new RegExp(r'[^\w\s]+'), '')}"));
 
       if (response.statusCode == 200) {
         final jsonResponse = json.decode(response.body);
@@ -42,161 +73,189 @@ class _PdfViewerState extends State<PdfViewer> {
         // Handle HTTP error
         return null;
       }
-    }catch(e){}
+    } catch (e) {}
   }
-
 
   void _showToast(BuildContext context, String text) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
       SnackBar(
-        duration: const Duration(seconds: 15),
-
+        duration: const Duration(seconds: 20),
         content: FutureBuilder<dynamic>(
           future: fetchAlbum(text),
-          builder:  (BuildContext context,AsyncSnapshot<dynamic> snapshot){
-            if(snapshot.hasError) {
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasError) {
               print("something has wrong");
-
             }
 
-            if(snapshot.connectionState==ConnectionState.waiting){
-              return Center(child: CircularProgressIndicator());
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
             }
             try {
               dynamic data = snapshot.data[0]["meanings"];
               dynamic definitions = data[0]["definitions"];
+              dynamic partOfSpeech = data[0]['partOfSpeech'];
+              dynamic synonyms = data[0]["synonyms"];
 
-
-              return Column(
-
-                children: [
-
-                  Text("Definitions 1 : ${definitions[0]['definition']!}"),
-                  SizedBox(height: 6,),
-                  Text("Definitions 2 : ${definitions[1]['definition']!}"),
-                  SizedBox(height: 6,),
-                  Text("Definitions 3 : ${definitions[2]['definition']!}"),
-
-                ],
+              return SizedBox(
+                width: 180,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "partOfSpeech :${partOfSpeech!}",
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    if (synonyms != null) ...[
+                      Text(
+                        "synonyms :$synonyms",
+                        maxLines: 4,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                    ],
+                    if (definitions[1]['definition'] != null) ...[
+                      Text(
+                        "definition :${definitions[1]['definition']!}",
+                        maxLines: 4,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                    ],
+                    if (definitions[0]['example'] != null) ...[
+                      Text(
+                        "example :${definitions[0]['example']!}",
+                        maxLines: 4,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                    ],
+                    if (definitions[0]['definition'] != null) ...[
+                      Text(
+                        "definition :${definitions[0]['definition']!}",
+                        maxLines: 4,
+                      ),
+                    ]
+                  ],
+                ),
               );
-            }catch(e){
-              return Column(
-
+            } catch (e) {
+              return const Column(
                 children: [
-
                   Text("Api responce is null"),
-
                 ],
               );
             }
-
-
           },
         ),
-        action: SnackBarAction(label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+        action: SnackBarAction(
+            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
   }
 
-
-  void HandlePickedFile()async{
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom,
-      allowedExtensions: ['pdf'],);
+  void HandlePickedFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
 
     if (result != null) {
       File file = File(result.files.single.path!);
       setState(() {
-
         bytes = file.readAsBytesSync();
-        isFilePicked=true;
-       });
-
+        isFilePicked = true;
+      });
     } else {
       print("picked fail");
       // User canceled the picker
     }
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
 
+    return Scaffold(
+      appBar: AppBar(
+        title: InkWell(child:Text('PDF Viewer'),onTap: (){Navigator.pop(context);},),
+        actions: <Widget>[
+          isSearch
+              ? SizedBox(
+            height: 30,
+                width: 200,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 0),
+                  child: TextField(
+                    controller: Controller,
+                    onSubmitted: (text){
+                      _showToast(context,text );
+                    },
 
+                    ),
+                ),
+              )
+              : IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () async {
+                    if (selectedTextLines != null) {
 
-
-
-    return  Scaffold(
-        appBar: AppBar(
-          title: const Text('PDF Viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () async {
-
-                if (selectedTextLines != null) {
-                  print(selectedTextLines);
-                  try {
-
-
-                    if (kDebugMode) {
-
-                      _showToast(context, selectedTextLines!);
+                      try {
+                        _showToast(context, selectedTextLines!);
+                      } catch (e) {
+                      }
+                    } else {
+                      setState(() {
+                        isSearch = true;
+                      });
 
                     }
-                  } catch (e) {
-                    if (kDebugMode) {
-                      print(e);
-                    }
-                  }
-
-
-                }
-              },
-            ),
-          ],
-        ),
-
-        body: isFilePicked? Container(
-
-          child: SfPdfViewer.memory(
+                    ;
+                  })
+        ],
+      ),
+      body: isFilePicked
+          ? SfPdfViewer.memory(
             bytes,
-            pageSpacing: 1,
+            pageSpacing: 4,
             key: _pdfViewerKey,
             controller: pdfViewerController,
-            onTextSelectionChanged: (PdfTextSelectionChangedDetails text){
-              String? Text= text.selectedText;
-              selectedTextLines=Text;
+            onTextSelectionChanged: (PdfTextSelectionChangedDetails text) {
+              String? Text = text.selectedText;
+              selectedTextLines = Text;
+            },
+            onTap: (PdfGestureDetails){
+              setState(() {
+                isSearch=false;
+                Controller.clear();
+              });
 
             },
 
-            interactionMode: PdfInteractionMode.selection,
             canShowHyperlinkDialog: false,
             canShowPageLoadingIndicator: false,
             canShowPaginationDialog: false,
             enableDocumentLinkAnnotation: false,
-            initialScrollOffset: Offset.infinite,
 
 
-
-          ),
-        )
-            : Center(
+          )
+          : Center(
               heightFactor: 10,
               widthFactor: 10,
               child: ElevatedButton(
-                child: Text("Select .pdf file"),
+                child: const Text("Select .pdf file"),
                 onPressed: () {
-
                   HandlePickedFile();
-
-
                 },
               ),
             ),
-
-
     );
-
-
   }
 }
